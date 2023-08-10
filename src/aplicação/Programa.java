d8256dcf1ad928123e04cccfa99e37c20752a632
Alisson de Moura <alisson_moura_rj@hotmail.com>
2023-08-10 12:16:38 -0300

    Primeira classe: Posição

---
+++ src/aplicação/Programa.java
@@ -0,0 +1,13 @@
+package aplicação;
+
+import jogotabuleiro.Posição;
+
+public class Programa {
+	
+	public static void main(String[] args) {
+		
+		Posição posição = new Posição(3, 5);
+		
+		System.out.println(posição);
+	}
+}
