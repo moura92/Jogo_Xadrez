d6a1335d93de55df811f1f07d26f50acc3b821d6
Alisson de Moura <alisson_moura_rj@hotmail.com>
2023-08-11 16:29:14 -0300

    Implementando possiveis movimentos da Rei

--- src/xadrez/peças/Rei.java
+++ src/xadrez/peças/Rei.java
@@ -1,5 +1,6 @@
 package xadrez.peças;
 
+import jogotabuleiro.Posição;
 import jogotabuleiro.Tabuleiro;
 import xadrez.Cor;
 import xadrez.PeçaXadrez;
@@ -15,9 +16,65 @@ public class Rei extends PeçaXadrez {
 		return "R";
 	}
 
+	private boolean podeMover(Posição posição) {
+		PeçaXadrez p = (PeçaXadrez) getTabuleiro().peça(posição);
+		return p == null || p.getCor() != getCor();
+	}
+
 	@Override
 	public boolean[][] movimentoPossivel() {
 		boolean[][] matriz = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+
+		Posição p = new Posição(0, 0);
+
+		// verificar se tem posição Acima da peça(REI):
+		p.setValores(posição.getLinha() - 1, posição.getColuna());
+		if (getTabuleiro().existePosição(p) && podeMover(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// verificar se tem posição Abaixo da peça(REI):
+		p.setValores(posição.getLinha() + 1, posição.getColuna());
+		if (getTabuleiro().existePosição(p) && podeMover(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// verificar se tem posição a Esquerda da peça(REI):
+		p.setValores(posição.getLinha(), posição.getColuna() - 1);
+		if (getTabuleiro().existePosição(p) && podeMover(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// verificar se tem posição a Direita da peça(REI):
+		p.setValores(posição.getLinha(), posição.getColuna() + 1);
+		if (getTabuleiro().existePosição(p) && podeMover(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		// verificar se tem posição na diagonal(Seperior Esquerda) da peça(REI):
+		p.setValores(posição.getLinha() - 1, posição.getColuna() - 1);
+		if (getTabuleiro().existePosição(p) && podeMover(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		// verificar se tem posição na diagonal(Seperior Direita) da peça(REI):
+		p.setValores(posição.getLinha() - 1, posição.getColuna() + 1);
+		if (getTabuleiro().existePosição(p) && podeMover(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		// verificar se tem posição na diagonal(Inferior Esquerda) da peça(REI):
+		p.setValores(posição.getLinha() + 1, posição.getColuna() - 1);
+		if (getTabuleiro().existePosição(p) && podeMover(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		
+		// verificar se tem posição na diagonal(Inferior Direita) da peça(REI):
+		p.setValores(posição.getLinha() + 1, posição.getColuna() + 1);
+		if (getTabuleiro().existePosição(p) && podeMover(p)) {
+			matriz[p.getLinha()][p.getColuna()] = true;
+		}
 		return matriz;
 	}
 }
