4931e13a672d1b6c9c7570a902e5e5a3fba0ac91
Alisson de Moura <alisson_moura_rj@hotmail.com>
2023-08-14 21:06:57 -0300

    Corrigindo um erro

--- src/xadrez/peças/Rei.java
+++ src/xadrez/peças/Rei.java
@@ -7,7 +7,7 @@ import xadrez.PartidaXadrez;
 import xadrez.PeçaXadrez;
 
 public class Rei extends PeçaXadrez {
-	
+
 	private PartidaXadrez partidaXadrez;
 
 	public Rei(Tabuleiro tabuleiro, Cor cor, PartidaXadrez partidaXadrez) {
@@ -24,10 +24,10 @@ public class Rei extends PeçaXadrez {
 		PeçaXadrez p = (PeçaXadrez) getTabuleiro().peça(posição);
 		return p == null || p.getCor() != getCor();
 	}
-	
+
 	private boolean testeTorreRock(Posição posição) {
-		PeçaXadrez p = (PeçaXadrez)getTabuleiro().peça(posição);
-		return p != null &&  p instanceof Torre && p.getCor() == getCor() && p.getContagemMovimento() == 0;
+		PeçaXadrez p = (PeçaXadrez) getTabuleiro().peça(posição);
+		return p != null && p instanceof Torre && p.getCor() == getCor() && p.getContagemMovimento() == 0;
 	}
 
 	@Override
@@ -59,56 +59,56 @@ public class Rei extends PeçaXadrez {
 		if (getTabuleiro().existePosição(p) && podeMover(p)) {
 			matriz[p.getLinha()][p.getColuna()] = true;
 		}
-		
+
 		// verificar se tem posição na diagonal(Seperior Esquerda) da peça(REI):
 		p.setValores(posição.getLinha() - 1, posição.getColuna() - 1);
 		if (getTabuleiro().existePosição(p) && podeMover(p)) {
 			matriz[p.getLinha()][p.getColuna()] = true;
 		}
-		
+
 		// verificar se tem posição na diagonal(Seperior Direita) da peça(REI):
 		p.setValores(posição.getLinha() - 1, posição.getColuna() + 1);
 		if (getTabuleiro().existePosição(p) && podeMover(p)) {
 			matriz[p.getLinha()][p.getColuna()] = true;
 		}
-		
+
 		// verificar se tem posição na diagonal(Inferior Esquerda) da peça(REI):
 		p.setValores(posição.getLinha() + 1, posição.getColuna() - 1);
 		if (getTabuleiro().existePosição(p) && podeMover(p)) {
 			matriz[p.getLinha()][p.getColuna()] = true;
 		}
-		
-		
+
 		// verificar se tem posição na diagonal(Inferior Direita) da peça(REI):
 		p.setValores(posição.getLinha() + 1, posição.getColuna() + 1);
 		if (getTabuleiro().existePosição(p) && podeMover(p)) {
 			matriz[p.getLinha()][p.getColuna()] = true;
 		}
-		
-		//Movimento especial: Roque
+
+		// Movimento especial: Roque
 		if (getContagemMovimento() == 0 && !partidaXadrez.getCheck()) {
-			//Movimento especial: Roque do lado do rei(Roque pequeno)
-			Posição posiçãoTorre1 =  new Posição(posição.getLinha(), posição.getColuna() + 3);
+			// Movimento especial: Roque do lado do rei(Roque pequeno)
+			Posição posiçãoTorre1 = new Posição(posição.getLinha(), posição.getColuna() + 3);
 			if (testeTorreRock(posiçãoTorre1)) {
 				Posição p1 = new Posição(posição.getLinha(), posição.getColuna() + 1);
 				Posição p2 = new Posição(posição.getLinha(), posição.getColuna() + 2);
-				if ( getTabuleiro().peça(p1) == null && getTabuleiro().peça(p2) == null) {
+				if (getTabuleiro().peça(p1) == null && getTabuleiro().peça(p2) == null) {
 					matriz[posição.getLinha()][posição.getColuna() + 2] = true;
 				}
 			}
-			//Movimento especial: Roque do lado do rainha(Roque grande)
-			Posição posiçãoTorre2 =  new Posição(posição.getLinha(), posição.getColuna() - 4);
+			// Movimento especial: Roque do lado do rainha(Roque grande)
+			Posição posiçãoTorre2 = new Posição(posição.getLinha(), posição.getColuna() - 4);
 			if (testeTorreRock(posiçãoTorre2)) {
 				Posição p1 = new Posição(posição.getLinha(), posição.getColuna() - 1);
 				Posição p2 = new Posição(posição.getLinha(), posição.getColuna() - 2);
 				Posição p3 = new Posição(posição.getLinha(), posição.getColuna() - 3);
 
-				if ( getTabuleiro().peça(p1) == null && getTabuleiro().peça(p2) == null && getTabuleiro().peça(p3) == null) {
+				if (getTabuleiro().peça(p1) == null && getTabuleiro().peça(p2) == null
+						&& getTabuleiro().peça(p3) == null) {
 					matriz[posição.getLinha()][posição.getColuna() - 2] = true;
 				}
 			}
 		}
-		
+
 		return matriz;
 	}
 }
